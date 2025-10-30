578ae63a30cb5210b050e6afcae096bd1e0ca949
Eduardo Cerato <eduardo_luis@hotmail.com>
2025-10-29 22:43:56 -0300

    feat: add auth module, model, resolver and service

---
+++ backend/src/auth/auth.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthResolver } from './auth.resolver';
+import { JwtModule } from '@nestjs/jwt';
+
+@Module({
+  imports: [JwtModule.register({
+    secret: process.env.JWT_SECRET,
+    signOptions: { expiresIn: '1h' }
+  })],
+  providers: [AuthService, AuthResolver]
+})
+export class AuthModule { }
---
+++ backend/src/auth/auth.resolver.ts
@@ -0,0 +1,36 @@
+import { Resolver, Mutation, Args } from '@nestjs/graphql';
+import { AuthService } from './auth.service';
+import { AuthResponse } from './auth.model';
+
+@Resolver()
+export class AuthResolver {
+    constructor(
+        private authService: AuthService
+    ) { }
+
+    @Mutation(() => AuthResponse)
+    async register(@Args('email') email: string, @Args('password') password: string): Promise<AuthResponse> {
+        const user = await this.authService.register(email, password);
+        const accessToken = this.authService.generateToken(user);
+        return {
+            accessToken,
+            user: {
+                email: user.email,
+                id: user.id
+            }
+        };
+    }
+
+    @Mutation(() => AuthResponse)
+    async login(@Args('email') email: string, @Args('password') password: string): Promise<AuthResponse> {
+        const user = await this.authService.login(email, password);
+        const accessToken = this.authService.generateToken(user);
+        return {
+            accessToken,
+            user: {
+                email: user.email,
+                id: user.id
+            }
+        };
+    }
+}
