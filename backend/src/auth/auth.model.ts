af9239a65e9931c4adf56fee9be656711f4f7764
Eduardo Cerato <eduardo_luis@hotmail.com>
2025-10-30 01:04:14 -0300

    fix: fix bug on auth model were user was not implemented yet when used

--- backend/src/auth/auth.model.ts
+++ backend/src/auth/auth.model.ts
@@ -1,19 +1,19 @@
 import { ObjectType, Field } from '@nestjs/graphql';
 
 @ObjectType()
-export class AuthResponse {
-  @Field()
-  accessToken: string;
-
-  @Field(() => User)
-  user: User;
-}
-
-@ObjectType()
 export class User {
   @Field()
   email: string;
 
   @Field()
   id: string;
+}
+
+@ObjectType()
+export class AuthResponse {
+  @Field()
+  accessToken: string;
+
+  @Field(() => User)
+  user: User;
 }
